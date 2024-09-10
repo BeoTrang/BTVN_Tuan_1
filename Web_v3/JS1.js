63a1386c6f207c59f579c20dbab6ed25bc09f8ad
Phạm Quang Trường <beobeotrang27@gmail.com>
2024-09-10 15:24:05 +0700

    web_v3 = web_v2 + json

---
+++ Web_v3/JS1.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    function send_api() {
+        var data = {
+            n: $('#TB1').val()
+        };
+
+        if (data.n=="" || isNaN(data.n)) {
+            $('#KetQua').html('Nhập lại');
+            $('#TB1').focus();
+            return;
+        }
+
+        $('#KetQua').html('Đang tính toán...');
+        
+        $.post("api.aspx",data,
+            function (kq, status) {
+                var json = JSON.parse(kq);
+                if (json.ok) {
+                    var uoc = json.msg;
+                    var html = uoc;
+                    $('#KetQua').html(html);
+                }
+                else {
+                    $('#KetQua').html(hmtl);
+                }
+            }
+        );
+    }
+    $("#Button1").click(function () {
+        send_api();
+    });
+});
---
+++ Web_v3/api.aspx.cs
@@ -0,0 +1,48 @@
+﻿using DLL_Tinh;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website_v2
+{
+    public partial class api : System.Web.UI.Page
+    {
+        class PhanHoi
+        {
+            public bool ok;
+            public string msg;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PhanHoi p = new PhanHoi();
+            string kq = "";
+            try
+            {
+                int n, dem = 0;
+                n = int.Parse(this.Request.Form["n"]);
+                Tinh tinh = new Tinh();
+                tinh.n = n;
+                tinh.Uoc();
+                dem = tinh.dem;
+                kq = $"Kết quả: " + dem + "</a>";
+                p.ok = true;
+                p.msg = kq;
+            }
+            catch (Exception ex)
+            {
+                kq = $"Có lỗi: " + ex.Message;
+                p.ok = false;
+                p.msg = kq;
+            }
+            finally
+            {
+                string json = JsonConvert.SerializeObject(p);
+                this.Response.Write(json);
+            }
+        }
+    }
+}
